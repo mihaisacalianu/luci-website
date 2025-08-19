629fad9fe6f3662b06130494064ab0819bd4d57d
mihaisacalianu <mihaisacalianu@gmail.com>
2025-08-19 15:55:54 +0100

    Update ContactUsBanner layout for improved responsiveness

--- src/components/header/ContactUsBanner.tsx
+++ src/components/header/ContactUsBanner.tsx
@@ -2,7 +2,7 @@ import { Mail, Phone } from "lucide-react";
 
 export const ContactUsBanner = () => {
   return (
-    <section className="flex items-center justify-end pr-20 gap-4 p-2 bg-gray-100 border-b border-gray-200 ">
+    <section className="flex items-center justify-center md:justify-end md:pr-20 pr-0 gap-4 p-2 bg-gray-100 border-b border-gray-200 ">
       <div className="flex items-center space-x-2 text-sm text-gray-600">
         <Phone className="size-4" />
         <span>+44 7778 699357</span>
