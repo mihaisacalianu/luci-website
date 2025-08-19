ab84e8fdb59e0cc42911bb320e2d80610393f595
mihaisacalianu <mihaisacalianu@gmail.com>
2025-08-19 15:56:15 +0100

    Add Contact Us and HeroSection components

---
+++ src/app/landing/contact-us/page.tsx
@@ -0,0 +1,10 @@
+const ContactUs = () => {
+  return (
+    <main className="w-[80%] mx-auto h-screen">
+      {/* <h1>Contact Us Page!</h1> */}
+      <div className="flex justify-center items-center"></div>
+    </main>
+  );
+};
+
+export default ContactUs;
---
+++ src/app/landing/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1 className="mt-20">Lucian House Page</h1>;
+}
