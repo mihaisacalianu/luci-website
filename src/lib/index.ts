ccd601d1c6b5aa4a3d642ea17c071d2dd44d5eac
mihaisacalianu <mihaisacalianu@gmail.com>
2025-08-30 20:03:24 +0100

    Add constants and utility functions for mastic calculations

---
+++ src/lib/index.ts
@@ -0,0 +1,3 @@
+export * from "./constats";
+export * from "./utils";
+export * from "./number-of-cartiges";
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function calculateMastic(width:string,
+  depth:string,
+  length:string){
+  const sealantVolume =
+  parseFloat(width)
+  * parseFloat(depth)
+  * parseFloat(length)
+  * 1.0;
+
+  return sealantVolume;
+}
